6d8a38822ed2e432449b0c8accff21923d57e1f2
gyan-cell <jha07944@gmail.com>
2023-11-01 15:47:50 +0530

    Infix To Postfix converter

---
+++ structPointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{ 
+  // struct Node* node1 = (struct Node*)malloc(sizeof(struct Node));
+  // meaning of these line is pretty simple first a pointer variable is created named node1;
+  // it can access any part of struct obj and then malloc function is used to assign a 
+  // memory to the malloc function (Struct Node*) is The type 
+  struct Hello {
+    int roll ;
+  };
+  struct Hello* hello;
+  hello ->roll = 20;
+  printf("%d\n",hello->roll);
+}
