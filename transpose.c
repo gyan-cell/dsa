8b8afbed43ef64060deb81a5ff3f556cd99aa0c7
gyan-cell <jha07944@gmail.com>
2023-12-25 14:41:31 +0530

    Transpose of matrix

---
+++ transpose.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define N 3 // You can change the size of the matrix as needed
+
+int main() {
+    int matrix[N][N] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    printf("Original Matrix:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Finding the transpose by swapping elements across the main diagonal
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            // Swap the elements matrix[i][j] and matrix[j][i]
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    printf("\nTranspose Matrix:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
