89004d7fb452bd3a866ebec4ee953364f96098fb
gyan-cell <jha07944@gmail.com>
2024-10-28 21:52:12 +0530

    Binary Search In Python

---
+++ binarySearch.py
@@ -0,0 +1,27 @@
+
+
+array = [ 2, 3, 4, 10, 40 ]
+
+
+x= 3
+
+
+def binarySearch(x,array, low , high):
+    if high >= low:
+        mid = (high + low) // 2
+        if array[mid] == x:
+            return mid
+        elif array[mid] > x:
+            return binarySearch(x, array, low, mid - 1)
+        else:
+            return binarySearch(x, array, mid + 1, high)
+    else:
+        return -1
+
+
+result = binarySearch(x, array, 0, len(array) - 1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Element is not present in array")
